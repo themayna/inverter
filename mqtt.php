f104e9b4de6f473e48ea486ddac05e42fc949a25
a_mainerici <angel.mainerici@mindgeek.com>
2022-06-04 15:29:32 +0300

    null:

---
+++ mqtt.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+class Mqtt
+{
+
+    protected $mqtt;
+
+    public function __construct()
+    {
+        $this->mqtt = new \PhpMqtt\Client\MqttClient(host: 'solar-assistant.local');
+        $this->mqtt->connect();
+    }
+
+    public function subscribe()
+    {
+        $this->mqtt->subscribe('#', function ($topic, $message) {
+            echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
+        }, 0);
+        $this->mqtt->loop(true);
+        $this->mqtt->disconnect();
+    }
+}
+
+(new Mqtt())->subscribe();
