d00dd36f3b6ca1cf4552cd450b569447d66098fe
a_mainerici <angel.mainerici@mindgeek.com>
2022-06-08 22:24:51 +0300

    improvementuri

--- mqtt.php
+++ mqtt.php
@@ -7,20 +7,35 @@ class Mqtt
     protected $mqtt;
     protected $client;
     const HOST = 'petuniilor.go.ro';
+    protected $status;
 
     public function __construct()
     {
         $this->mqtt = new \PhpMqtt\Client\MqttClient(host: self::HOST);
         $this->client = new GuzzleHttp\Client();
+        $response = $this->client->request(
+            method: 'POST',
+            uri: self::HOST . ':35001/zeroconf/info',
+            options: [
+                'body' => json_encode(['deviceid', 'data' => []])
+            ]
+        );
+        $info = json_decode($response->getBody()->getContents());
+
+        $this->status = $info->data->switch = 'off' ? false : true;
+
         $this->mqtt->connect();
     }
 
     public function subscribe()
     {
-        $this->mqtt->subscribe('solar_assistant/total/battery_state_of_charge/state', function ($topic, $message) {
-
-            echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
-            if ($message == 75) {
+        $this->mqtt->subscribe('solar_assistant/total/battery_state_of_charge/state', function ($topic, $percent) {
+            echo sprintf(
+                "Battery percentage is %s and switch is tunned %s\n",
+                $percent,
+                $this->status == true ? "On" : "Off"
+            );
+            if ($percent == 32 && $this->status == false) {
                 //start la retea
                 $this->client->request(
                     method: 'POST',
@@ -29,18 +44,19 @@ class Mqtt
                         'body' => json_encode(['data' => ['switch' => 'on']])
                     ]
                 );
+                echo "Turned the grid ON";
             }
-//            if ($message == 75) {
-//                //stop la retea
-//                $this->client->request(
-//                    method: 'POST',
-//                    uri: self::HOST . ':35001/zeroconf/switch',
-//                    options: [
-//                        'body' => json_encode(['data' => ['switch' => 'off']])
-//                    ]
-//                );
-//            }
-
+            if ($percent == 40 && $this->status == true) {
+                //stop la retea
+                $this->client->request(
+                    method: 'POST',
+                    uri: self::HOST . ':35001/zeroconf/switch',
+                    options: [
+                        'body' => json_encode(['data' => ['switch' => 'off']])
+                    ]
+                );
+                echo "Turned the grid OFF";
+            }
         }, 0);
         $this->mqtt->loop();
         $this->mqtt->disconnect();
