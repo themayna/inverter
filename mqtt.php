bc6be8a31282b84b37f63d802e9de3546418c2de
a_mainerici <angel.mainerici@mindgeek.com>
2022-09-02 08:56:52 +0300

    new update

--- mqtt.php
+++ mqtt.php
@@ -12,6 +12,11 @@ class Mqtt
 
     public function __construct()
     {
+        $this->initConnection()->subscribe();
+    }
+
+    protected function initConnection()
+    {
         $this->mqtt = new \PhpMqtt\Client\MqttClient(host: self::HOST);
         $this->client = new GuzzleHttp\Client();
         $response = $this->client->request(
@@ -26,48 +31,55 @@ class Mqtt
         $this->switch = $info['data']['switch'] == 'on' ? true : false;
 
         $this->mqtt->connect();
+
+        return $this;
     }
 
     public function subscribe()
     {
-        $this->mqtt->subscribe('solar_assistant/inverter_1/device_mode/state', function ($topic, $message) {
-            $this->state = $message;
-        }, 0);
+        try {
+            $this->mqtt->subscribe('solar_assistant/inverter_1/device_mode/state', function ($topic, $message) {
+                $this->state = $message;
+            }, 0);
 
-        $this->mqtt->subscribe('solar_assistant/total/battery_state_of_charge/state', function ($topic, $percent) {
-            echo sprintf(
-                "Battery percentage is at %s and switch is tunned %s\n",
-                $percent,
-                $this->switch == true ? "On" : "Off"
-            );
-            if ($percent <= 32 && $this->switch == false) {
-                //start la retea
-                $this->client->request(
-                    method: 'POST',
-                    uri: self::HOST . ':35001/zeroconf/switch',
-                    options: [
-                        'body' => json_encode(['data' => ['switch' => 'on']])
-                    ]
+            $this->mqtt->subscribe('solar_assistant/total/battery_state_of_charge/state', function ($topic, $percent) {
+                echo sprintf(
+                    "Battery percentage is at %s and switch is tunned %s\n",
+                    $percent,
+                    $this->switch == true ? "On" : "Off"
                 );
-                $this->switch = true;
-                echo "Turned the grid ON\n";
-            }
-            if ($percent >= 35 && $this->switch == true && $this->state == 'Solar/Battery') {
-                //stop la retea
-                $this->client->request(
-                    method: 'POST',
-                    uri: self::HOST . ':35001/zeroconf/switch',
-                    options: [
-                        'body' => json_encode(['data' => ['switch' => 'off']])
-                    ]
-                );
-                $this->switch = false;
-                echo "Turned the grid OFF\n";
-            }
-        }, 0);
-        $this->mqtt->loop();
-        $this->mqtt->disconnect();
+                if ($percent <= 35 && $this->switch == false) {
+//                  start la retea
+                    $this->client->request(
+                        method: 'POST',
+                        uri: self::HOST . ':35001/zeroconf/switch',
+                        options: [
+                            'body' => json_encode(['data' => ['switch' => 'on']])
+                        ]
+                    );
+                    $this->switch = true;
+                    echo "Turned the grid ON\n";
+                }
+                if ($percent >= 40 && $this->switch == true && $this->state == 'Solar/Battery') {
+//                    stop la retea
+                    $this->client->request(
+                        method: 'POST',
+                        uri: self::HOST . ':35001/zeroconf/switch',
+                        options: [
+                            'body' => json_encode(['data' => ['switch' => 'off']])
+                        ]
+                    );
+                    $this->switch = false;
+                    echo "Turned the grid OFF\n";
+                }
+            }, 0);
+            $this->mqtt->loop();
+            $this->mqtt->disconnect();
+        } catch (Throwable $exception) {
+            echo sprintf($exception->getMessage());
+            $this->initConnection()->subscribe();
+        }
     }
 }
 
-(new Mqtt())->subscribe();
+(new Mqtt());
